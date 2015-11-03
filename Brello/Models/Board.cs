740dd285ed1708b98f441cddf8bc91d7a7dfc399
jlsears <jen.lauren.sears@gmail.com>
2015-11-02 21:32:24 -0600

    Creating boards, etc.

--- Brello/Models/Board.cs
+++ Brello/Models/Board.cs
@@ -9,7 +9,17 @@ namespace Brello.Models
     {
         public int BoardId { get; set; }
         public string Title { get; set; }
-        public virtual ICollection<BrelloList> Lists { get; set; }
-        public virtual ICollection<ApplicationUser> Followers { get; set; }
+        public ApplicationUser Owner { get; set; }
+
+        // Changed from ICollection to List (List implements ICollection)
+        public virtual List<BrelloList> Lists { get; set; }
+        public virtual List<ApplicationUser> Followers { get; set; }
+
+        // Constructor to allow for initializing in the test
+        public Board()
+        {
+            Lists = new List<BrelloList>();
+            Followers = new List<ApplicationUser>();
+        }
     }
 }
---
+++ Brello/Models/BoardRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Brello.Models
+{
+    public class BoardRepository
+    {
+        private BoardContext context;
+        public BoardRepository(BoardContext _context) {
+            context = _context;
+        }
+
+        // void or bool or BrelloList
+        public bool AddList(Board _board, BrelloList _list)
+        {
+            return false;
+        }
+
+        public List<BrelloList> GetAllLists()
+        {
+            return null;
+        }
+
+        // This is an example of overloading a method
+        public List<BrelloList> GetAllLists(Board _board)
+        {
+            return null;
+        }
+
+        public Board CreateBoard(string title, ApplicationUser owner)
+        {
+            Board my_board = new Board { Title = title, Owner = owner };
+            context.Boards.Add(my_board);
+            context.SaveChanges(); // This saves something to the database
+
+            return my_board;
+        }
+    }
+}
