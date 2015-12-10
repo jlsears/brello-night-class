2b227e9c88b61447a09553141f3dd0c2ccb9f6c1
jlsears <jen.lauren.sears@gmail.com>
2015-12-09 21:05:46 -0600

    Angular Wednesday night

---
+++ Brello/Scripts/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('Brello', []);
+
+app.controller("BoardController", ["$scope", "$http", function ($scope, $http) {
+    $scope.test = "Hello World!";
+
+    $scope.hello = function () {
+        //$scope.test = "Hello, is it me you're looking for?";
+        // GET: api/BoardApi/
+        // How make ajax call using angular?
+        $http.get("api/BoardApi")
+            .success(function (data) {
+                $scope.test = data;
+        })
+        .error(function () { alert(error.message);})
+    }
+}]);
